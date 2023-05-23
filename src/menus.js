b115746b54adaba98a9da0e1e1b3c598b4534007
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-05-23 07:00:07 +0300

    finishing menu

--- src/menus.js
+++ src/menus.js
@@ -1,10 +1,30 @@
 const createMenus = () => {
-  // const menusHeading = document.createElement('h3');
-  // const menuList = document.createElement('ul');
 
-  // const menuListOne = document.createElement('li')
-  // menuList.textContent = "one";
-  // menuList.appendChild(menuList);
+  const menusWrapper = document.createElement('div');
+  menusWrapper.classList.add('menus-wrapper');
+
+  const menusHeading = document.createElement('h3');
+  menusHeading.innerText = 'Menus';
+  menusWrapper.appendChild(menusHeading);
+
+  const menuList = document.createElement('ul');
+  menusWrapper.appendChild(menuList)
+
+  const menuListOne = document.createElement('li');
+  menuListOne.textContent = 'first menu'
+  menuList.appendChild(menuListOne);
+  const menuListTwo = document.createElement('li');
+  menuListTwo.textContent = 'second menu'
+  menuList.appendChild(menuListTwo);
+  const menuListThree = document.createElement('li');
+  menuListThree.textContent = 'third menu'
+  menuList.appendChild(menuListThree);
+  const menuListFour = document.createElement('li');
+  menuListFour.textContent = 'fourth menu'
+  menuList.appendChild(menuListFour);
+
+  content.appendChild(menusWrapper)
+
 }
 
 export default createMenus;
